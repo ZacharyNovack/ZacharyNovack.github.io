64c447c341efb2f12e712b5340b3719ad1b95630
Zachary Novack <znovack@Eorlingas.local>
2023-01-29 23:45:45 -0800

    study

--- study/follow_371/full_source/follow.js
+++ study/follow_371/full_source/follow.js
@@ -2761,7 +2761,7 @@ function addTips () {
 }
 
 $(document).ready (function () {
-    //inFileName = document.url.split("/")[4].split(".")[0];
+    inFileName = document.url.split("/")[4];
     audElm = document.getElementById ('aud');
     volElm = document.getElementById ('vol') || { addEventListener: () => {} }; // dummy object als schuifregelaar niet bestaat
     perfElm = document.getElementById ('perf');
